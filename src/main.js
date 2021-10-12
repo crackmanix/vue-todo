06601a985e34b0f322fbb1a704d67d4986b94e4c
crackmanix <crackmanix@gmail.com>
2021-10-12 17:05:36 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import './plugins/async-computed'
-import './plugins/vue-moment'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import '@/plugins/bootstrap-vue'
+import '@/plugins/async-computed'
+import '@/plugins/vue-moment'
+import App from '@/App.vue'
+import store from '@/store'
+import router from '@/router'
 
 Vue.config.productionTip = false
 
