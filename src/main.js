1e309a2abf2ddf61c3fa0722e1627d0236cd463a
crackmanix <crackmanix@gmail.com>
2021-10-12 18:20:41 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import '@/plugins/bootstrap-vue'
 import '@/plugins/async-computed'
 import '@/plugins/vue-moment'
