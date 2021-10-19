609d12424b2c3f2bc021f8aa960a379a41f465a2
crackmanix <crackmanix@gmail.com>
2021-10-19 18:11:11 +0200

    init

--- src/components/taskModal.vue
+++ src/components/taskModal.vue
@@ -1,70 +1,84 @@
 <template>
-  <b-modal :id="getModalId" v-model="modalShow">
+  <b-modal :id="modalId" v-model="modalShow" @cancel="cancel" @ok="validate" cancel-variant="danger"
+           ok-variant="success">
     <template v-slot:modal-title>
-      Tâche N°{{task.id}}
+      Tâche N°{{ task.id }}
     </template>
-    <b-container fluid>
-      <b-row>
-        <b-col>
-          <label for="titre">Titre de la tâche</label>
-          <b-form-input id="titre"
-                        v-model="task.title"
-                        placeholder="Acheter des pâtes ...">
-          </b-form-input>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <label for="description">Description de la tâche</label>
-          <b-form-textarea id="description"
-                           v-model="task.description"
-                           placeholder="Acheter des linguines et de la sauce carbonara."
-                           rows="3"
-                           max-rows="6">
-          </b-form-textarea>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <label for="etat">Tâche accomplie ?</label>
-          <b-form-checkbox id="etat"
-                           v-model="task.completed"
-                           :value="true"
-                           :unchecked-value="false"
-                           switch
-                           size="lg">
-          </b-form-checkbox>
-        </b-col>
-      </b-row>
-    </b-container>
-    <template v-slot:modal-footer>
-      <b-button variant="success">
-        <b-icon-check></b-icon-check> Valider
-      </b-button>
-      <b-button variant="danger">
-       <b-icon-exclamation></b-icon-exclamation> Annuler
-      </b-button>
+    <b-form>
+      <b-container fluid>
+        <b-row>
+          <b-col>
+            <label for="titre">Titre de la tâche</label>
+            <b-form-input id="titre"
+                          v-model="task.title"
+                          placeholder="Acheter des pâtes ...">
+            </b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <label for="description">Description de la tâche</label>
+            <b-form-textarea id="description"
+                             v-model="task.description"
+                             placeholder="Acheter des linguines et de la sauce carbonara."
+                             rows="3"
+                             max-rows="6">
+            </b-form-textarea>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <label for="etat">Tâche accomplie ?</label>
+            <b-form-checkbox id="etat"
+                             v-model="task.completed"
+                             switch
+                             size="lg">
+            </b-form-checkbox>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-form>
+    <template v-slot:modal-ok>
+      <b-icon-check></b-icon-check>
+      Valider
+    </template>
+    <template v-slot:modal-cancel>
+      <b-icon-exclamation></b-icon-exclamation>
+      Annuler
     </template>
   </b-modal>
 </template>
 
 <script>
+import {mapGetters, mapMutations} from "vuex";
+
 export default {
   name: "taskModal",
-  props: ['task'],
   data() {
     return {
-      modalShow: false
+      modalShow: false,
+      modalId: 'taskModal',
+      task: {}
     }
   },
-  computed:{
-    getModalId(){
-      return 'taskModal' + this.task.id;
-    }
+  computed: {
+    ...mapGetters(["getTask"])
   },
   methods: {
-    hideModal() {
-      this.$refs[this.getModalId].hide();
+    ...mapMutations(["setTask"]),
+    cancel() {
+      this.setTask({});
+    },
+    validate() {
+      this.setTask(this.task);
+    }
+  },
+  watch: {
+    getTask: {
+      handler() {
+        this.task = this.getTask;
+      },
+      immediate: true
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,39 +7,43 @@
     </b-row>
     <b-row>
       <b-col class="text-center">
-        <task-modal :task="{id:''}"></task-modal>
-        <b-button variant="success" class="m-3" @click="$bvModal.show('taskModal')">
+        <b-button variant="success" class="m-3" @click="addTask()">
           <b-icon-bookmark-plus-fill></b-icon-bookmark-plus-fill>
-          Add Task</b-button>
+          Add Task
+        </b-button>
       </b-col>
     </b-row>
     <b-row>
       <b-col class="text-center">
         <b-table hover striped :fields="fields" :items="todoList">
           <template v-slot:cell(completed)="data">
-            <b-icon-check-circle-fill variant="success" v-if="data.value" :title="data.value.toString()"></b-icon-check-circle-fill>
-            <b-icon-exclamation-circle-fill variant="danger" v-else :title="data.value.toString()"></b-icon-exclamation-circle-fill>
+            <b-icon-check-circle-fill variant="success" v-if="data.value"
+                                      :title="data.value.toString()"></b-icon-check-circle-fill>
+            <b-icon-exclamation-circle-fill variant="danger" v-else
+                                            :title="data.value.toString()"></b-icon-exclamation-circle-fill>
           </template>
           <template v-slot:cell(actions)="data">
-            <task-modal :task="data.item"></task-modal>
-            <b-button variant="warning" @click="$bvModal.show('taskModal'+data.item.id)" class="mr-2">
+            <b-button variant="warning" @click="updateTask(data.item)" class="mr-2">
               <b-icon-pencil-fill></b-icon-pencil-fill>
-              Update</b-button>
+              Update
+            </b-button>
             <b-button variant="danger" class="ml-2">
               <b-icon-trash-fill></b-icon-trash-fill>
-              Delete</b-button>
+              Delete
+            </b-button>
           </template>
         </b-table>
       </b-col>
     </b-row>
+    <task-modal></task-modal>
   </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
 import Top from '@/components/top'
 import todolistService from "@/services/todoListService";
 import TaskModal from "@/components/taskModal";
+import {mapGetters, mapMutations} from "vuex";
 
 export default {
   name: 'Home',
@@ -57,6 +61,9 @@ export default {
       ]
     }
   },
+  computed:{
+    ...mapGetters(["getTask"])
+  },
   asyncComputed: {
     todoList: {
       get() {
@@ -66,12 +73,23 @@ export default {
       },
       default() {
         return [
-            {id:1, completed: true, description: 40, title: 'Dickerson'},
-            {id:2, completed: false, description: 21, title: 'Larsen'},
-            {id:3, completed: false, description: 89, title: 'Geneva'},
-            {id:4, completed: true, description: 38, title: 'Jami'}
+          {id: 1, completed: true, description: 40, title: 'Dickerson'},
+          {id: 2, completed: false, description: 21, title: 'Larsen'},
+          {id: 3, completed: false, description: 89, title: 'Geneva'},
+          {id: 4, completed: true, description: 38, title: 'Jami'}
         ];
       }
+    }
+  },
+  methods:{
+    ...mapMutations(["setTask"]),
+    addTask(){
+      this.setTask({id: null});
+      this.$bvModal.show('taskModal');
+    },
+    updateTask(task){
+      this.setTask({...task});
+      this.$bvModal.show('taskModal');
     }
   }
 }
